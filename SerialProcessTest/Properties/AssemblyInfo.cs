4487773e6b06d13ccb04451c378e5cfbd2d57ac2
sarumugam <shenbagaraj.arumugam@viasat.com>
2023-02-08 15:07:54 +0530

    Test class added

---
+++ SerialProcessTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerialProcessTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SerialProcessTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c43d8fe-98ca-4035-863e-8ab658912786")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SerialProcessTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SerialProcessTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly SerialProcess.SerialProcess _serialProcess;
+
+        public UnitTest1()
+        {
+            _serialProcess = new SerialProcess.SerialProcess();
+        }
+
+        [TestMethod, Priority(1), TestCategory("CategoryA")]
+        public void AddTest1()
+        {
+            int result = _serialProcess.Add(10, 20);
+            Assert.AreEqual(result, 30);
+        }
+
+        [TestMethod, Priority(1), TestCategory("CategoryB")]
+        public void AddTest2()
+        {
+            int result = _serialProcess.Add((-10), 20);
+            Assert.AreEqual(result, 10);
+        }
+
+        [TestMethod, Priority(2), TestCategory("CategoryA")]
+        public void AddTest3()
+        {
+            int result = _serialProcess.Add(0, 20);
+            Assert.AreEqual(result, 20);
+        }
+
+        [TestMethod, Priority(2), TestCategory("CategoryB")]
+        public void AddTest4()
+        {
+            int result = _serialProcess.Add(int.MaxValue, 20);
+            Assert.AreEqual(result, 20);
+        }
+    }
+}
