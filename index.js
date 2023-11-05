9cf84772c2fd41fb6037cedf5375a29ce24a2099
Arumugam, Shenbagaraj <Shenbagaraj.Arumugam@viasat.com>
2023-11-05 12:30:40 +0530

    Test commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let obj = {
+    name: 'Raj',
+    age: 65
+};
+
+
+
+console.log(obj);
+
+
+
+let selectedColors = ['Red', 'Blue', 2];
+
+function greet(number) {
+
+    return number * number;
+
+}
+
+console.log(greet('Test'));
